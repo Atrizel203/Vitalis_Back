e07a410a313154d8656d4a246f623d02271e2aa9
ATRIZEL203 <darkangel_066@hotmail.com>
2024-11-25 07:15:22 -0600

    no message

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Importar CORS
 const sequelize = require('./src/config/database.config');
 const Paciente = require('./src/models/Paciente');
 const Expediente = require('./src/models/Expediente');
@@ -16,15 +17,28 @@ const Usuario = require('./src/models/usuario.model');
 
 dotenv.config();
 const app = express();
+
+// Configuración de CORS
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Cambia a la URL de tu frontend (e.g., http://localhost:3001)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Habilita el envío de cookies
+};
+app.use(cors(corsOptions)); // Usar CORS con las opciones definidas
+
+// Middleware para parsear JSON
 app.use(express.json());
 
+// Rutas
 app.use('/api/auth', authRoutes);
 app.use('/api/pacientes', pacienteRoutes);
 app.use('/api/expedientes', expedienteRoutes);
-app.use('/api/signos-vitales', signosVitalesRoutes); 
+app.use('/api/signos-vitales', signosVitalesRoutes);
 app.use('/api/analisis', analisisRoutes);
 app.use('/api/medicos', medicoRoutes);
 
+// Sincronización de modelos
 async function syncModels() {
   try {
     await Paciente.sync();
@@ -33,24 +47,29 @@ async function syncModels() {
     await SignosVitales.sync();
     await Analisis.sync();
     await Usuario.sync();
-    
     console.log('Sincronización de modelos exitosa');
   } catch (error) {
     console.error('Error al sincronizar los modelos:', error);
   }
 }
 
-sequelize.authenticate().then(() => {
-  console.log('Conexión a la base de datos exitosa');
-  syncModels();
-}).catch(err => {
-  console.error('No se pudo conectar a la base de datos:', err);
-});
+// Conexión a la base de datos
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos exitosa');
+    syncModels();
+  })
+  .catch((err) => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
 
+// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('API de expediente médico');
 });
 
+// Inicio del servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en ${PORT}`);
